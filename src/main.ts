194f127d99eb4ae4aeaa3dc8d80a1f2e176a449f
Edward Mizuka <mizuka.wu@outlook.com>
2024-05-05 14:51:45 +0800

    feat: 日志布局 --user=yiwei.wu

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div id="app_container">
     <h1><img src="${typescriptLogo}" />Online Web Digital Watermarking</h1>
     <div>
       <input id="input" type="file" accept="image/gif, image/png, image/jpg, image/jpeg, image/svg" />
+    </div>
+
+    <div id="info">
+      <div id="result">
+        <h3>encoded</h3>
+        <div id="encode_result"></div>
+        <h3>decoded</h3>
+        <div id="decode_result"></div>
+      </div>
+      <div id="logs">
+        <h3>Logs</h3>
+        <textarea readonly></textarea>
+      </div>
     </div>
   </div>
 `
